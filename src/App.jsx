0cce19de0c560d294483918f63c149e7d1071942
Sam Sh <saby24@student.bth.se>
2025-09-11 14:44:55 +0200

    Add header and css-style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Docs from "./pages/Docs.jsx";
+import Header from "./components/Header.jsx";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Docs />
+      <Router>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Docs />} />
+          </Routes>
+        </main>
+      </Router>
     </>
   );
 }
