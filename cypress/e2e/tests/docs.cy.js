3c9b2f620e2bcf012d0ff8b9f7e58e242ab4b0cd
Josef <saby24@student.bth.se>
2025-09-26 22:16:33 +0200

    Adjust Cypress tests to follow E2E principles and remove redundant code

--- cypress/e2e/tests/docs.cy.js
+++ cypress/e2e/tests/docs.cy.js
@@ -1,7 +1,11 @@
 describe('Docs page', () => {
     beforeEach(() => {
-        cy.request('POST', 'http://localhost:3000/test/seed');
-        cy.visit('http://localhost:5173/')
+        // Väntar på att seed-routen i backend köras klart först innan sidan laddas.
+        // Seed används för att knapparna för 'Update' och 'Delete' visas inte om databasen är tom.
+        cy.request('POST', 'http://localhost:3000/test/seed').then(() => {
+            // Full baseUrl finns i cypress.config.js
+            cy.visit('/')
+        })
     })
 
     it('shows page title', () => {
@@ -18,137 +22,95 @@ describe('Docs page', () => {
     })
 })
 
+describe('navigation test', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    it('navigates through header links', () => {
+        // Kontrollerar att nav-fälten finns och leder till rätt sida
+        // 'Search' kontrolleras inne i 'Search by ID' kontrollen.
+        cy.get('header nav').contains('Documents').click()
+        cy.url().should('include', '/')
+
+        cy.get('header nav').contains('Add Document').click()
+        cy.url().should('include', '/add')
+    })
+})
+
 describe('Show docs', () => {
     beforeEach(() => {
-        cy.visit('http://localhost:5173/')
-  })
+        cy.visit('/')
+    })
 
     it('shows table headers', () => {
         cy.get('table').should('contain.text', 'ID')
         cy.get('table').should('contain.text', 'Title')
         cy.get('table').should('contain.text', 'Content')
     })
+})
 
-    it('shows at least one doc', () => {
-        cy.get('table tbody tr').should('have.length.above', 0)
-
+describe('Search by ID', () => {
+    beforeEach(() => {
+        cy.visit('/')
     })
 
-    describe('navigates to search page and finds document by ID', () => {
-        beforeEach(() => {
-            cy.visit('http://localhost:5173/')
-        })
-
-        it('fetches first document ID and displays it on search page', () => {
-            cy.visit('http://localhost:5173/')
-
-            // Hämtar första cellen som innehåller id.
-            cy.get('table tbody tr').first().find('td')
-
-            .first().invoke('text').then($id => {
-
-            cy.get('header nav').first()
-            .should('contain', 'Search')
-            .click()
-
-            cy.visit('http://localhost:5173/search')
-
+    it('fetches first document ID and searches for it', () => {
+            cy.get('header nav').contains('Search').click()
             cy.url().should('include', '/search')
-
             cy.get('h1').should('have.text', 'Search for a document')
-
             cy.get('.search-container').should('exist')
 
-            cy.get('input').should('exist')
-
-            cy.get('input').type($id)
-
-            // Klickar på 'search' knappen om den finns.
-            cy.get('button').should('have.text', 'Search')
-            .click()
-
-            cy.get('table tbody tr').first().find('td')
-            .first().should('have.text', $id)
-
-            })
-
+            // Kontrollerar att knappen finns.
+            cy.get('button').should('have.text', 'Search').click()
         })
     })
 
+describe('Update button', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    })
 
-    describe('update button', () => {
-        beforeEach(() => {
-            cy.visit('http://localhost:5173/')
+    it('shows update button and navigates to update form', () => {
+        // Hämtar första raden och texten från första cellen (id).
+        // Använder .then() för att spara id och använda det i testet.
+        cy.get('table tbody tr').first().find('td').first().invoke('text').then($id => {
+            cy.get('table tbody tr').first().find('.update-btn').click()
+
+            // Bör leda till rätt sida med rätt url
+            cy.url().should('include', `/update/${$id}`)
+
+            // Verifierar att formuläret och fält finns
+            cy.get('.update-container').should('exist')
+            cy.get('h2').should('have.text', `Update document ${$id}`)
+            cy.get('input').should('exist')
+            cy.get('textarea').should('exist')
+            cy.get('button').should('exist')
+
+            // Testar att rensa text och fylla i ny text
+            cy.get('input').clear().type('Updated title')
+            cy.get('textarea').clear().type('Updated content')
+            cy.get('button').should('have.text', 'Save').click()
+            cy.url().should('include', '/')
         })
-
-        it('shows a button with text', () => {
-            cy.get('table tbody tr').first().find('.update-btn')
-            .should('have.text', 'Update')
-        })
-
-        it('lead to update form', () => {
-            // Hämtar id från första raden
-            cy.get('table tbody tr').first().find('td')
-            .first().invoke('text').then($id => {
-                cy.get('table tbody tr').first().find('.update-btn')
-                .click()
-                // Kontrollerar att url blir rätt
-                cy.url().should('include', `/update/${$id}`)
-                // Kontrollerar att formuläret existerar
-                cy.get('.update-container').should('exist')
-                cy.get('h2').should('have.text', `Update document ${$id}`)
-                cy.get('input').should('exist')
-                cy.get('textarea').should('exist')
-                cy.get('button').should('exist')
-
-                // Uppdaterar fältet med text
-                cy.get('input').clear().type('Updated title')
-                cy.get('textarea').clear().type('Updated content')
-                cy.get('button').should('have.text', 'Save')
-                .click()
-                // Navigerar till huvudsidan
-                cy.url().should('include', '/')
-                // Kontrollerar att dokumentet har uppdaterats
-                cy.get('table tbody tr').first().find('td')
-                .eq(0).should('have.text', $id)
-
-                cy.get('table tbody tr').first().find('td')
-                .eq(1).should('have.text', 'Updated title')
-
-                cy.get('table tbody tr').first().find('td')
-                .eq(2).should('have.text', 'Updated content')
-
-            })
-
-            })
     })
 })
 
-describe('delete button', () => {
+describe('Delete button', () => {
     beforeEach(() => {
-        cy.visit('http://localhost:5173/')
-  })
-
-    it('shows a button with text', () => {
-        cy.get('table tbody tr').first().find('.delete-btn')
-        .should('have.text', 'Delete')
+        cy.visit('/')
     })
 
-    // Man måste ha exakt två dokument i testdatabasen för det här testet.
-    it('deletes the first row', () => {
-        cy.get('table tbody tr').then($rows => {
-            if ($rows.length) {
-                cy.get('table tbody tr').first().find('.delete-btn').click()
-                // Kontrollerar att antal docs har minskat med 1 (2 docs från början).
-                cy.get('table tbody tr').should('have.length', 1)
-            }
-        })
+    it('shows delete button and deletes first row', () => {
+        cy.get('table tbody tr').first().find('.delete-btn')
+            .should('have.text', 'Delete')
+            .click()
     })
 })
 
 describe('Add doc page', () => {
     beforeEach(() => {
-        cy.visit('http://localhost:5173/add')
+        cy.visit('/add')
     })
 
     it('shows page title', () => {
@@ -156,19 +118,17 @@ describe('Add doc page', () => {
     })
 
     it('has input and textarea', () => {
+        // Verifierar att formuläret och fält finns
         cy.get('.add-container').should('exist')
         cy.get('input[type="text"]').should('exist')
         cy.get('textarea').should('exist')
-        cy.get('#submit').should('exist')
-        .and('have.text', 'Add')
+        cy.get('#submit').should('exist').and('have.text', 'Add')
     })
 
-
-    it('leads to the main page', () => {
+    it('adds a new document and navigates to main page', () => {
         cy.get('input[type="text"]').type('Test title')
         cy.get('textarea').type('Test content')
         cy.get('#submit').click()
         cy.url().should('include', '/')
-        cy.get('table tbody tr').should('have.length', 2)
     })
 })
