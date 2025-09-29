5040f8b6a050e3b283d1572ec6d057703752ac64
Hampe <hampe024@gmail.com>
2025-09-29 20:21:18 +0200

    Update vite.config.js
    fix URL error for local development

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/react-project/',
+  base: command === 'serve' ? '/' : '/react-project/',
   server: {
     watch: {
       usePolling: true,
     },
   },
-})
+}))
