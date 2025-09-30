54bfec698b0b121adecdb662a4f348ba7747a4ec
Hampe <hampe024@gmail.com>
2025-09-30 16:25:38 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'serve' ? '/' : '/react-project/',
+  base: command === 'serve' ? '/' : '/react-project',
   server: {
     watch: {
       usePolling: true,
